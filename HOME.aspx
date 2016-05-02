de9e5a9ef81d4afda89b3ba7be801044fa718685
Jiaqi Weng <jwengab@ust.hk>
2016-05-02 23:40:39 +0800

    Added files via upload
    aaa

---
+++ HOME.aspx
@@ -0,0 +1,94 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="HOME.aspx.cs" Inherits="HKeInvestWebApplication.HOME" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Home Page</h2>
+
+    <div class="form-group">
+        <div class="col-md-4">
+            <asp:Label ID="type" runat="server" Text="Type"></asp:Label>
+            <asp:DropDownList ID="ddlType" runat="server">
+                <asp:ListItem>--Select a type of security--</asp:ListItem>
+                <asp:ListItem>Bond</asp:ListItem>
+                <asp:ListItem>UnitTrust</asp:ListItem>
+                <asp:ListItem>Stock</asp:ListItem>
+            </asp:DropDownList>
+            <asp:RequiredFieldValidator ControlToValidate="ddlType" InitialValue="--Select a type of security--" runat="server" Display="Dynamic" CssClass="text-danger" EnableClientScript="false" ErrorMessage="Must select a type of security"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-md-4">
+            <asp:CheckBox ID="CheckBoxName" runat="server" OnCheckedChanged="CheckBoxName_CheckedChanged" AutoPostBack="true" />
+            <asp:Label ID="LabelName" runat="server" Text="Name"></asp:Label>
+            <asp:TextBox ID="TextBoxName" runat="server" CssClass="form-control"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredName" ControlToValidate="TextBoxName" runat="server" ErrorMessage="Name of security is required." Display="Dynamic" CssClass="text-danger" EnableClientScript="false" Enabled="false"></asp:RequiredFieldValidator>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-md-4">
+            <asp:CheckBox ID="CheckBoxCode" runat="server" OnCheckedChanged="CheckBoxCode_CheckedChanged" AutoPostBack="true" />
+            <asp:Label ID="LabelCode" runat="server" Text="Code"></asp:Label>
+            <asp:TextBox ID="TextBoxCode" runat="server" CssClass="form-control"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredCode" ControlToValidate="TextBoxCode" runat="server" ErrorMessage="Code of security is required." Display="Dynamic" CssClass="text-danger" EnableClientScript="false" Enabled="false"></asp:RequiredFieldValidator>
+            <asp:RegularExpressionValidator runat="server" CssClass="text-danger" Display="Dynamic" ControlToValidate="TextBoxCode" ValidationExpression="^[0-9]{0,}$" EnableClientScript="false" ErrorMessage="Must be digital"></asp:RegularExpressionValidator>
+            <asp:CustomValidator ID="CustomCode" ControlToValidate="TextBoxCode" CssClass="text-danger" Display="Dynamic" EnableClientScript="false" runat="server" ErrorMessage="The security code must contain 1-4 effective digits." OnServerValidate="CustomCode_ServerValidate"></asp:CustomValidator>
+        </div>
+    </div>
+
+
+    <div class="form-group">
+        <div class="col-md-offset-2 col-md-10">
+            <asp:Button ID="search" runat="server" Text="Search" Height="34px" CssClass="btn button-default" OnClick="search_Click" />
+        </div>
+    </div>
+
+    <div>
+        <asp:GridView ID="gvStock" runat="server" AutoGenerateColumns="false" Visible="false" AllowSorting="true" OnSorting="gvStock_Sorting">
+            <Columns>
+                <asp:BoundField DataField="code" HeaderText="Code" ReadOnly="true" SortExpression="code" />
+                <asp:BoundField DataField="name" HeaderText="Name" ReadOnly="true" SortExpression="name" />
+                <asp:BoundField DataField="close" HeaderText="Recent Price" ReadOnly="true" SortExpression="close" />
+                <asp:BoundField DataField="changePercent" HeaderText="Price change in percentage" ReadOnly="true" SortExpression="changePercent" />
+                <asp:BoundField DataField="changeDollar" HeaderText="Price change in dollar" ReadOnly="true" SortExpression="changeDollar" />
+                <asp:BoundField DataField="volume" HeaderText="Trade Volume" ReadOnly="true" SortExpression="volume" />
+                <asp:BoundField DataField="high" HeaderText="High Price" ReadOnly="true" SortExpression="high" />
+                <asp:BoundField DataField="low" HeaderText="Low Price" ReadOnly="true" SortExpression="low" />
+                <asp:BoundField DataField="peRatio" HeaderText="Price Earning Ratio" ReadOnly="true" SortExpression="peRatio" />
+                <asp:BoundField DataField="yield" HeaderText="Yield of Stock" ReadOnly="true" SortExpression="yield" />
+            </Columns>
+        </asp:GridView>
+    </div>
+    <div>
+        <asp:GridView ID="gvBond" runat="server" AutoGenerateColumns="false" Visible="false" AllowSorting="true" OnSorting="gvBond_Sorting">
+            <Columns>
+                <asp:BoundField DataField="code" HeaderText="Code" ReadOnly="true" SortExpression="code" />
+                <asp:BoundField DataField="name" HeaderText="Name" ReadOnly="true" SortExpression="name" />
+                <asp:BoundField DataField="base" HeaderText="Base Currency" ReadOnly="true" SortExpression="base" />
+                <asp:BoundField DataField="size" HeaderText="Size of Bond" ReadOnly="true" SortExpression="size" />
+                <asp:BoundField DataField="price" HeaderText="Current Price" ReadOnly="true" SortExpression="price" />
+                <asp:BoundField DataField="sixMonths" HeaderText="Compound Annual Growth in 6 months" ReadOnly="true" SortExpression="sixMonths" />
+                <asp:BoundField DataField="oneYear" HeaderText="Compound Annual Growth in one year" ReadOnly="true" SortExpression="oneYear" />
+                <asp:BoundField DataField="threeYears" HeaderText="Compound Annual Growth in 3 years" ReadOnly="true" SortExpression="threeYears" />
+                <asp:BoundField DataField="sinceLaunch" HeaderText="Compound Annual Growth Since Launch" ReadOnly="true" SortExpression="sinceLaunch" />
+
+            </Columns>
+        </asp:GridView>
+    </div>
+    <div>
+        <asp:GridView ID="gvUnitTrust" runat="server" AutoGenerateColumns="false" Visible="false" AllowSorting="true" OnSorting="gvUnitTrust_Sorting">
+            <Columns>
+                <asp:BoundField DataField="code" HeaderText="Code" ReadOnly="true" SortExpression="code" />
+                <asp:BoundField DataField="name" HeaderText="Name" ReadOnly="true" SortExpression="name" />
+                <asp:BoundField DataField="base" HeaderText="Base Currency" ReadOnly="true" SortExpression="base" />
+                <asp:BoundField DataField="size" HeaderText="Size of Bond" ReadOnly="true" SortExpression="size" />
+                <asp:BoundField DataField="price" HeaderText="Current Price" ReadOnly="true" SortExpression="price" />
+                <asp:BoundField DataField="riskReturn" HeaderText="Risk Return" ReadOnly="true" SortExpression="riskReturn" />
+                <asp:BoundField DataField="sixMonths" HeaderText="Compound Annual Growth in 6 months" ReadOnly="true" SortExpression="sixMonths" />
+                <asp:BoundField DataField="oneYear" HeaderText="Compound Annual Growth in one year" ReadOnly="true" SortExpression="oneYear" />
+                <asp:BoundField DataField="threeYears" HeaderText="Compound Annual Growth in 3 years" ReadOnly="true" SortExpression="threeYears" />
+                <asp:BoundField DataField="sinceLaunch" HeaderText="Compound Annual Growth Since Launch" ReadOnly="true" SortExpression="sinceLaunch" />
+            </Columns>
+        </asp:GridView>
+    </div>
+
+
+</asp:Content>
---
+++ HOME.aspx.cs
@@ -0,0 +1,450 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using HKeInvestWebApplication.Code_File;
+using HKeInvestWebApplication.ExternalSystems.Code_File;
+using Microsoft.AspNet.Identity;
+
+namespace HKeInvestWebApplication
+{
+    public partial class HOME : System.Web.UI.Page
+    {
+        ExternalData myExternalData = new ExternalData();
+        HKeInvestCode myHKeInvestCode = new HKeInvestCode();
+        ExternalFunctions myExternalFunctions = new ExternalFunctions();
+        private static Boolean updated = false;
+        private static DataTable dtView;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+           
+
+        }
+
+        protected void CheckBoxName_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CheckBoxName.Checked == true)
+            {
+                RequiredName.Enabled = true;
+                RequiredCode.Enabled = false;
+
+                CheckBoxCode.Checked = false;
+            }
+
+        }
+
+        protected void CheckBoxCode_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CheckBoxCode.Checked == true)
+            {
+                RequiredCode.Enabled = true;
+                RequiredName.Enabled = false;
+                CheckBoxName.Checked = false;
+            }
+        }
+
+        
+
+        protected void search_Click(object sender, EventArgs e)
+        {
+
+            DataTable dtType = null;
+            if (IsValid == false)
+            {
+                dtType = null;
+                gvStock.DataSource = dtType;
+                gvStock.DataBind();
+
+                gvStock.Visible = true;
+                
+                return;
+            }
+            UnloadGV();
+            string securityType = ddlType.SelectedItem.ToString().Trim();
+            string securityName = "-1";
+            string securityCode = "-1";
+            
+            if (CheckBoxName.Checked == true)
+            {
+                securityName = TextBoxName.Text.ToString().Trim();
+                string newName = securityName;
+                int count = 0;
+                securityName = securityName.Replace("'", "''");
+                string sql;
+                
+                if (securityType == "Stock")
+                {
+                    sql = "select * from[" + securityType + "] where [name] like '%" + securityName + "%' ";
+                    dtType = myExternalData.getData(sql);
+                    if (dtType == null)
+                    {
+                        return;
+                    }
+
+                    if (dtType.Rows.Count == 0)
+                    {
+                        return;
+                    }
+                    ViewState["SortExpression"] = "name";
+                    ViewState["SortDirection"] = "ASC";
+                    dtView = dtType.Clone();
+                    for (int i = 0; i < dtType.Rows.Count; i++)
+                    {
+                        dtView.ImportRow(dtType.Rows[i]);
+                    }
+                    //
+                    DataView sortedView = new DataView(dtView);
+                    sortedView.Sort = "name" + " " + "Asc";
+                    gvStock.DataSource = sortedView;
+
+                    gvStock.DataBind();
+
+                    gvStock.Visible = true;
+                    
+
+                }
+                else if (securityType == "Bond")
+                {
+                    sql = "select * from[" + securityType + "] where [name] like '%" + securityName + "%' ";
+                    dtType = myExternalData.getData(sql);
+                    if (dtType == null)
+                    {
+                        return;
+                    }
+
+                    if (dtType.Rows.Count == 0)
+                    {
+                        return;
+                    }
+                    ViewState["SortExpression"] = "name";
+                    ViewState["SortDirection"] = "ASC";
+                    dtView = dtType.Clone();
+                    for (int i = 0; i < dtType.Rows.Count; i++)
+                    {
+                        dtView.ImportRow(dtType.Rows[i]);
+                    }
+                    //
+                    DataView sortedView = new DataView(dtView);
+                    sortedView.Sort = "name" + " " + "Asc";
+                    gvBond.DataSource = sortedView;
+
+                    gvBond.DataBind();
+
+                    gvBond.Visible = true;
+                    
+                }
+                else
+                {
+                    sql = "select * from[" + securityType + "] where [name] like '%" + securityName + "%' ";
+                    dtType = myExternalData.getData(sql);
+                    if (dtType == null)
+                    {
+                        return;
+                    }
+
+                    if (dtType.Rows.Count == 0)
+                    {
+                        return;
+                    }
+                    ViewState["SortExpression"] = "name";
+                    ViewState["SortDirection"] = "ASC";
+                    dtView = dtType.Clone();
+                    for (int i = 0; i < dtType.Rows.Count; i++)
+                    {
+                        dtView.ImportRow(dtType.Rows[i]);
+                    }
+                    //
+                    DataView sortedView = new DataView(dtView);
+                    sortedView.Sort = "name" + " " + "Asc";
+                    gvUnitTrust.DataSource = sortedView;
+
+                    gvUnitTrust.DataBind();
+
+                    gvUnitTrust.Visible = true;
+                    
+                }
+                
+                
+            }
+            else if (CheckBoxCode.Checked == true)
+            {
+                securityCode = TextBoxCode.Text.ToString().Trim();
+                int count = 0;
+                foreach (Char c in securityCode.ToCharArray())
+                {
+                    if (c == '0')
+                    {
+                        count++;
+                    }
+                    else break;
+                }
+                securityCode = securityCode.Substring(count, securityCode.Length - count);
+                string sql;
+                
+                if (securityType == "Stock")
+                {
+                    sql = "select * from [" + securityType + "] where [code] = '" + securityCode + "' ";
+                    dtType = myExternalData.getData(sql);
+                    if (dtType == null)
+                    {
+                        return;
+                    }
+
+                    if (dtType.Rows.Count == 0)
+                    {
+                        return;
+                    }
+                    ViewState["SortExpression"] = "name";
+                    ViewState["SortDirection"] = "ASC";
+                    dtView = dtType.Clone();
+                    for (int i = 0; i < dtType.Rows.Count; i++)
+                    {
+                        dtView.ImportRow(dtType.Rows[i]);
+                    }
+                    //
+                    DataView sortedView = new DataView(dtView);
+                    sortedView.Sort = "name" + " " + "Asc";
+                    gvStock.DataSource = sortedView;
+
+                    gvStock.DataBind();
+
+                    gvStock.Visible = true;
+                    
+
+                }
+                else if (securityType == "Bond")
+                {
+                    sql = "select * from [" + securityType + "] where [code] = '" + securityCode + "' ";
+                    dtType = myExternalData.getData(sql);
+                    if (dtType == null)
+                    {
+                        return;
+                    }
+
+                    if (dtType.Rows.Count == 0)
+                    {
+                        return;
+                    }
+                    ViewState["SortExpression"] = "name";
+                    ViewState["SortDirection"] = "ASC";
+                    dtView = dtType.Clone();
+                    for (int i = 0; i < dtType.Rows.Count; i++)
+                    {
+                        dtView.ImportRow(dtType.Rows[i]);
+                    }
+                    //
+                    DataView sortedView = new DataView(dtView);
+                    sortedView.Sort = "name" + " " + "Asc";
+                    gvBond.DataSource = sortedView;
+
+                    gvBond.DataBind();
+
+                    gvBond.Visible = true;
+                    
+                }
+                else
+                {
+                    sql = "select * from [" + securityType + "] where [code] = '" + securityCode + "' ";
+                    dtType = myExternalData.getData(sql);
+                    if (dtType == null)
+                    {
+                        return;
+                    }
+
+                    if (dtType.Rows.Count == 0)
+                    {
+                        return;
+                    }
+                    ViewState["SortExpression"] = "name";
+                    ViewState["SortDirection"] = "ASC";
+                    dtView = dtType.Clone();
+                    for (int i = 0; i < dtType.Rows.Count; i++)
+                    {
+                        dtView.ImportRow(dtType.Rows[i]);
+                    }
+                    //
+                    DataView sortedView = new DataView(dtView);
+                    sortedView.Sort = "name" + " " + "Asc";
+                    gvUnitTrust.DataSource = sortedView;
+
+                    gvUnitTrust.DataBind();
+
+                    gvUnitTrust.Visible = true;
+                    
+                }
+
+
+            }
+            else
+            {
+                string sql;
+                //sql = "select * from [" + securityType + "] where ";
+                
+
+                if (securityType == "Stock")
+                {
+                    sql = "select * from [" + securityType + "]";
+                    
+                    dtType = myExternalData.getData(sql);
+                    if (dtType == null)
+                    {
+                        return;
+                    }
+
+                    if (dtType.Rows.Count == 0)
+                    {
+                        return;
+                    }
+                    ViewState["SortExpression"] = "name";
+                    ViewState["SortDirection"] = "ASC";
+                    dtView = dtType.Clone();
+                    for (int i = 0; i < dtType.Rows.Count; i++)
+                    {
+                        dtView.ImportRow(dtType.Rows[i]);
+                    }
+                    //
+                    DataView sortedView = new DataView(dtView);
+                    sortedView.Sort = "name" + " " + "Asc";
+                    gvStock.DataSource = sortedView;
+
+                    gvStock.DataBind();
+
+                    gvStock.Visible = true;
+                    
+
+                }
+                else if (securityType == "Bond")
+                {
+                    sql = "select * from [" + securityType + "] ";
+                    dtType = myExternalData.getData(sql);
+                    if (dtType == null)
+                    {
+                        return;
+                    }
+
+                    if (dtType.Rows.Count == 0)
+                    {
+                        return;
+                    }
+                    ViewState["SortExpression"] = "name";
+                    ViewState["SortDirection"] = "ASC";
+                    dtView = dtType.Clone();
+                    for (int i = 0; i < dtType.Rows.Count; i++)
+                    {
+                        dtView.ImportRow(dtType.Rows[i]);
+                    }
+                    //
+                    DataView sortedView = new DataView(dtView);
+                    sortedView.Sort = "name" + " " + "Asc";
+                    gvBond.DataSource = sortedView;
+
+                    gvBond.DataBind();
+
+                    gvBond.Visible = true;
+                    
+                }
+                else
+                {
+                    sql = "select * from [" + securityType + "] ";
+                    dtType = myExternalData.getData(sql);
+                    if (dtType == null)
+                    {
+                        return;
+                    }
+
+                    if (dtType.Rows.Count == 0)
+                    {
+                        return;
+                    }
+                    ViewState["SortExpression"] = "name";
+                    ViewState["SortDirection"] = "ASC";
+                    dtView = dtType.Clone();
+                    for (int i = 0; i < dtType.Rows.Count; i++)
+                    {
+                        dtView.ImportRow(dtType.Rows[i]);
+                    }
+                    //
+                    DataView sortedView = new DataView(dtView);
+                    sortedView.Sort = "name" + " " + "Asc";
+                    gvUnitTrust.DataSource = sortedView;
+
+                    gvUnitTrust.DataBind();
+
+                    gvUnitTrust.Visible = true;
+                    
+                }
+            }
+            
+        }
+        protected void UnloadGV()
+        {
+            gvStock.Visible = false;
+            gvBond.Visible = false;
+            gvUnitTrust.Visible = false;
+        }
+
+      
+
+        
+
+        
+
+        
+
+        protected void CustomCode_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            
+            string securityCode = TextBoxCode.Text.ToString().Trim();
+            int count = 0;
+            foreach (Char c in securityCode.ToCharArray())
+            {
+                if (c == '0')
+                {
+                    count++;
+                }
+                else break;
+            }
+            int length = securityCode.Length - count;
+            if (length > 4)
+            {
+                args.IsValid = false;
+
+            }
+            else args.IsValid = true;
+            
+        }
+
+        
+
+       
+
+        protected void gvStock_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            DataView sortedView = new DataView(dtView);
+            sortedView.Sort = e.SortExpression + " " + "Asc";
+            gvStock.DataSource = sortedView;
+            gvStock.DataBind();
+        }
+
+        protected void gvBond_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            DataView sortedView = new DataView(dtView);
+            sortedView.Sort = e.SortExpression + " " + "Asc";
+            gvBond.DataSource = sortedView;
+            gvBond.DataBind();
+        }
+
+        protected void gvUnitTrust_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            DataView sortedView = new DataView(dtView);
+            sortedView.Sort = e.SortExpression + " " + "Asc";
+            gvUnitTrust.DataSource = sortedView;
+            gvUnitTrust.DataBind();
+        }
+    }
+}
